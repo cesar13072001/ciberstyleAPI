a6c642ab95e2ba1444b042b1164cbfef9ec3edf9
Cesar Aguilar Zambrano <cesar13072001@gmail.com>
2022-11-09 20:54:28 -0500

    Cambios con salto de lineas

--- ciberstyleAPI/Controllers/ConsultaController.cs
+++ ciberstyleAPI/Controllers/ConsultaController.cs
@@ -26,8 +26,8 @@ namespace ciberstyleAPI.Controllers
 
                 foreach (var item in compras as List<compradetalle_Result>)
                 {
-                    totalT = "Total: S/. "+item.total;
-                    prendas += "*Producto: " + item.nombre + "*\n cantidad: " + item.cantidad + "\nsubtotal: S/. " + item.subtotal + "\n\n ";
+                    totalT = "\nTotal: S/. "+item.total;
+                    prendas += "*Producto: " + item.nombre + "*\n cantidad: " + item.cantidad + "\nsubtotal: S/. " + item.subtotal + ".\n ";
                 }
                 salida = salida + prendas + totalT;
                 result.resultado = salida;
